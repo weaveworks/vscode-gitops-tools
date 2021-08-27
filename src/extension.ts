bc1452c81f04039134dbb5a7801ae96f96bfdb8f
usernamehw <usernamehw@gmail.com>
2021-08-27 05:29:29 -0400

    Stub cluster tree view provider
    #22

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,18 @@
 import {
-  ExtensionContext,
-  window
+	ExtensionContext,
+	window
 } from 'vscode';
+import { ClusterTreeViewDataProvider } from './views/clusterTreeViewDataProvider';
 import { LinkTreeViewDataProvider } from './views/linkTreeViewDataProvider';
 import { Views } from './views/views';
 
 export function activate(context: ExtensionContext) {
-	// create documentation links sidebar tree view section
+  // create clusters tree view section
+  window.createTreeView(Views.ClusterView, {
+    treeDataProvider: new ClusterTreeViewDataProvider(),
+    showCollapseAll: true,
+  });
+  // create documentation links sidebar tree view section
 	window.createTreeView(Views.DocumentationView, {
 		treeDataProvider: new LinkTreeViewDataProvider(),
 		showCollapseAll: true,
