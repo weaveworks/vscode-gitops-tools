3b0b795c54ac66476ad46e3a113481d3d607ca9c
Taras Novak <taras.novak@randomfractals.com>
2021-09-10 10:17:53 -0500

    document treeViewItem.ts base class methods (#59)
    - add todo to revert options param change in constructor back to original typed params

--- src/views/treeViewItem.ts
+++ src/views/treeViewItem.ts
@@ -6,10 +6,18 @@ import {
   Uri
 } from 'vscode';
 
+/**
+ * Defines tree view item base class used by all GitOps tree views.
+ */
 export class TreeViewItem extends TreeItem {
   parent: TreeViewItem | undefined;
   children: TreeViewItem[] = [];
 
+	/**
+	 * Creates new tree view item.
+	 * TODO: change params back to typed params this class had originally.
+	 * @param options Tree view item options.
+	 */
   constructor({
     label,
 		description,
@@ -35,21 +43,37 @@ export class TreeViewItem extends TreeItem {
     }
   }
 
+	/**
+	 * Collapses tree node and hides its children.
+	 */
   makeCollapsible() {
     this.collapsibleState = TreeItemCollapsibleState.Collapsed;
   }
 
+	/**
+	 * Expands a tree node and shows its children.
+	 */
   expand() {
     this.collapsibleState = TreeItemCollapsibleState.Expanded;
   }
 
+	/**
+	 * Sets tree view item icon.
+	 * @param icon Theme icon, uri or light/dark svg icon path.
+	 */
   setIcon(icon: ThemeIcon | Uri | {light: string; dark: string}) {
     this.iconPath = icon;
   }
 
-  addChild(item: TreeViewItem) {
-    this.children.push(item);
+	/**
+	 * Add new tree view item to the children collection.
+	 * @param child Child tree view item to add.
+	 * @returns Updated tree view itme with added child.
+	 */
+  addChild(child: TreeViewItem) {
+    this.children.push(child);
     if (this.children.length) {
+			// update collapse/expand state
       if (this.collapsibleState !== TreeItemCollapsibleState.Expanded) {
         this.makeCollapsible();
       }
