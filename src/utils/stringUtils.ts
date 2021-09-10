f95f8f23d8e417500e2aeb87957fd2b67b3f0fa1
Taras Novak <taras.novak@randomfractals.com>
2021-09-10 08:59:33 -0500

    document, format, and refine shortenRevision() in stringUtils.ts (#40)

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,12 +1,15 @@
 /**
- * Return the first 7 symbols of SHA.
- * Git SHA includes the branch name.
+ * Shortens revision string for display in GitOps tree views.
+ * @param revision Revision string to shorten.
+ * @returns Short revision string with max 7 characters.
  */
-export function shortenRevision(sha = ''): string {
-	if (sha.includes('/')) {
-		const [gitBranch, gitSha] = sha.split('/');
-		return [gitBranch, '/', gitSha.slice(0, 7)].join('');
-	} else {
-		return sha.slice(0, 7);
+export function shortenRevision(revision: string = ''): string {
+	if (revision.includes('/')) {
+		// git revision includes branch name
+		const [gitBranch, gitRevision] = revision.split('/');
+		return [gitBranch, '/', gitRevision.slice(0, 7)].join('');
+	}
+	else {
+		return revision.slice(0, 7);
 	}
 }
