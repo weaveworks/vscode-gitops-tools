52bb8496cb9f427cc5a628810f68765a4e4c3cb3
Taras Novak <taras.novak@randomfractals.com>
2021-09-11 17:17:28 -0500

    add kubernetesFileSchemes.ts for creating virtual k8s object fs URIs (#60)

---
+++ src/kubernetes/kubernetesFileSchemes.ts
@@ -0,0 +1,13 @@
+/**
+ * Defines Kubernetes file schemes
+ * used by the virtual Kubernetes Tools file system provider
+ * to open kubernetes object config files in vscode editor.
+ * @see https://github.com/Azure/vscode-kubernetes-tools/blob/master/src/kuberesources.virtualfs.ts
+ */
+export enum KubernetesFileSchemes {
+	Resource = 'k8smsx',
+	ReadonlyResource = 'k8smsxro',
+	KubectlResource = 'loadkubernetescore',
+	DescribeResource = 'kubernetesdescribe',
+	HelmResource = 'helmget'
+}
