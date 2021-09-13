3a63df5abccb237424015252c3154e47bf277b9f
al <usernamehw@gmail.com>
2021-09-13 13:13:11 +0300

    add jsdoc descriptions

--- src/utils/markdownStringUtils.ts
+++ src/utils/markdownStringUtils.ts
@@ -3,6 +3,7 @@ import { MarkdownString } from 'vscode';
 /**
  * Create specific table header (2 columns, left aligned)
  * for the tree view item hover.
+ * @param markdown object of vscode type MarkdownString
  */
 export function generateMarkdownTableHeader(markdown: MarkdownString) {
 	markdown.appendMarkdown('Property | Value\n');
@@ -10,6 +11,9 @@ export function generateMarkdownTableHeader(markdown: MarkdownString) {
 }
 /**
  * Create markdown table row (only if value is not equal `undefined`)
+ * @param propertyName First table column value
+ * @param propertyValue Second table column value
+ * @param markdown object of vscode type MarkdownString
  */
 export function generateMarkdownTableRow(propertyName: string, propertyValue: string | boolean | undefined, markdown: MarkdownString) {
 	if (propertyValue === undefined) {
