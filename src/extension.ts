1636bf66f918f7304f5a436f4759a9ccea379244
al <usernamehw@gmail.com>
2021-12-22 12:26:41 +0300

    implement telemetry later (#187)

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,18 @@
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext, extensions } from 'vscode';
+import TelemetryReporter from 'vscode-extension-telemetry';
 import { registerCommands } from './commands';
 import { setExtensionContext } from './extensionContext';
 import { checkIfOpenedFolderGitRepositorySourceExists } from './git/checkIfOpenedFolderGitRepositorySourceExists';
 import { checkPrerequisites, promptToInstallFlux } from './install';
 import { statusBar } from './statusBar';
 import { createTreeViews } from './views/treeViews';
+
+// TODO: implement
+// const extensionId = 'weaveworks.vscode-gitops-tools';
+// const extensionVersion = extensions.getExtension(extensionId)?.packageJSON.version || 'unknown version';
+// const key = 'da19a1446ba2-369b-0484-b857-e706cf38'.split('').reverse().join('');
+
+// let reporter;
 
 /**
  * Activates GitOps extension.
@@ -28,6 +36,11 @@ export async function activate(context: ExtensionContext) {
 	// depending if the current opened folder is a git repository and already added
 	// to the cluster
 	checkIfOpenedFolderGitRepositorySourceExists();
+
+
+	// reporter = new TelemetryReporter(extensionId, extensionVersion, key);
+	// reporter.sendTelemetryEvent('Something info',	{'stringProp': 'some string' }, { 'numericMeasure': 123 });
+	// context.subscriptions.push(reporter);
 
 	// show error notification if flux is not installed
 	await promptToInstallFlux();
