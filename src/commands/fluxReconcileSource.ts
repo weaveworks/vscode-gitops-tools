e63eb832dc0be1623678f8c6baada68f86b7c18a
Kingdon Barrett <kingdon@weave.works>
2023-07-24 12:26:17 -0400

    remove unused code
    don't leave it hanging around in comments, it's not coming back
    
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- src/commands/fluxReconcileSource.ts
+++ src/commands/fluxReconcileSource.ts
@@ -7,7 +7,6 @@ import { BucketNode } from 'ui/treeviews/nodes/source/bucketNode';
 import { GitRepositoryNode } from 'ui/treeviews/nodes/source/gitRepositoryNode';
 import { HelmRepositoryNode } from 'ui/treeviews/nodes/source/helmRepositoryNode';
 import { OCIRepositoryNode } from 'ui/treeviews/nodes/source/ociRepositoryNode';
-import { refreshSourcesTreeView } from 'ui/treeviews/treeViews';
 
 /**
  * Invoke flux reconcile of a specific source.
@@ -26,6 +25,4 @@ export async function fluxReconcileSourceCommand(source: GitRepositoryNode | OCI
 	}
 
 	await fluxTools.reconcile(sourceType, source.resource.metadata?.name || '', source.resource.metadata?.namespace || '');
-
-	// refreshSourcesTreeView();
 }
