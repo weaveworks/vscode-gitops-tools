eea0f036a4e9a52205f15abb47458dcd4364ef0a
Kingdon Barrett <kingdon@weave.works>
2023-07-31 15:02:31 -0400

    remember to add debug message here
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- src/commands/fluxCheckPrerequisites.ts
+++ src/commands/fluxCheckPrerequisites.ts
@@ -6,6 +6,9 @@ import { enabledFluxChecks } from 'extension';
  */
 export async function checkFluxPrerequisites() {
 	if(enabledFluxChecks()) {
+		// Missing debug message here
+		// Ref: https://github.com/weaveworks/vscode-gitops-tools/pull/459
+		// whenever we're skipping a call to `flux check` we should emit a skippable ...
 		return await shell.execWithOutput('flux check --pre');
 	} else {
 		return true;
