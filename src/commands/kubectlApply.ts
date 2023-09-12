8837e28d37910d72c0b3cf0ccb410afa4487421d
Juozas Gaigalas <juozasgaigalas@gmail.com>
2023-09-12 23:27:50 +0300

    kubectl apply open file

--- src/commands/kubectlApply.ts
+++ src/commands/kubectlApply.ts
@@ -1,12 +1,14 @@
 import * as shell from 'cli/shell/exec';
-import { Uri } from 'vscode';
+import { Uri, window } from 'vscode';
 
 export async function kubectlApplyPath(uri?: Uri) {
+	uri ??= window.activeTextEditor?.document.uri;
 	if(uri) {
 		return await shell.execWithOutput(`kubectl apply -f ${uri.fsPath}`);
 	}
 }
 export async function kubectlDeletePath(uri?: Uri) {
+	uri ??= window.activeTextEditor?.document.uri;
 	if(uri) {
 		return await shell.execWithOutput(`kubectl delete -f ${uri.fsPath}`);
 	}
