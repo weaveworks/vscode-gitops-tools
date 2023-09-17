6d88e45925ae27943e8972fc87515a5c319fc1e3
Seth Falco <seth@falco.fun>
2023-09-17 17:23:53 +0100

    feat: recommend settings for log viewer

--- src/commands/showLogs.ts
+++ src/commands/showLogs.ts
@@ -1,9 +1,9 @@
-import { commands, window } from 'vscode';
+import { ConfigurationTarget, commands, window, workspace } from 'vscode';
 
 import { getPodsOfADeployment } from 'cli/kubernetes/kubectlGet';
+import { ResourceNode, podResourceKind } from 'types/showLogsTypes';
 import { ClusterDeploymentNode } from 'ui/treeviews/nodes/cluster/clusterDeploymentNode';
 import { getResourceUri } from 'utils/getResourceUri';
-import { ResourceNode, podResourceKind } from 'types/showLogsTypes';
 
 /**
  * Show logs in the editor webview (running Kubernetes extension command)
@@ -31,4 +31,32 @@ export async function showLogs(deploymentNode: ClusterDeploymentNode): Promise<v
 	};
 
 	commands.executeCommand('extension.vsKubernetesLogs', podResourceNode);
+
+	const vscGitopsConfig = workspace.getConfiguration('gitops');
+	if (vscGitopsConfig.get('ignoreConfigRecommendations')) {
+		return;
+	}
+
+	const vscKubeConfig = workspace.getConfiguration('vscode-kubernetes.log-viewer');
+	if (vscKubeConfig.get('autorun') && vscKubeConfig.get('follow')) {
+		return;
+	}
+
+	window.showInformationMessage(
+		'It\'s recommended to autorun and follow logs by default. Do you want to apply these settings?',
+		'Apply Settings',
+		'Never Show Again',
+	).then(result => {
+		if (!result) {
+			return;
+		}
+
+		if (result === 'Never Show Again') {
+			workspace.getConfiguration('gitops').update('ignoreConfigRecommendations', true, ConfigurationTarget.Global);
+			return;
+		}
+
+		vscKubeConfig.update('autorun', true, ConfigurationTarget.Global);
+		vscKubeConfig.update('follow', true, ConfigurationTarget.Global);
+	});
 }
